a646934f257330b9073ced77fad048fe3e38d623
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-06 19:45:35 +0000

    container config done

---
+++ javascript/testcase.js
@@ -0,0 +1,8 @@
+console.log("kartik")
+const accountid =4444
+let email ="kartikkaushik1693@gmail.com"
+accountcity ="rohtak"
+acountholder =" divyam"
+accountage =" 10"
+accountstatement = " verified"
+console.table([accountcity,acountholder,accountage,accountstatement])
