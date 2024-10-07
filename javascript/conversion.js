5c656e73f88bdd7a6b4c5e8efdc312436a4dfee5
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-07 16:11:07 +0000

    conversion

--- javascript/conversion.js
+++ javascript/conversion.js
@@ -1,11 +1,31 @@
-let score ="33abc"
+//let score ="33abc"
 //console.log( typeof score)
 //console.log(score)
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 //console.log(typeof valueInNumber )
 //console.log(score)
-let key ="kartik"
+/*let key ="kartik"
 let booleankey =Boolean(key)
 console.log(key)
 console.log(booleankey)
 // 1 =true,0= false,"" =false,"kartik" = true
+let numbber =33
+let stringnubber =String(numbber)
+console.log(stringnubber)
+console.log(typeof stringnubber)
+console.log(1+2)
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"3")
+console.log("1"+2+3)
+//console.log( +true)
+//console.log(true+)
+console.log(+"")
+console.log("2" == 2)
+console.log("2" === 2)
+console.log(null<0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(undefined<0)
+console.log(undefined > 0)
+console.log(undefined >= 0)*/
