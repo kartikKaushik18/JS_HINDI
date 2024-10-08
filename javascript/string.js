e4264886a43148100b7337433286f9307937b3bd
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-08 20:23:59 +0000

    string

--- javascript/string.js
+++ javascript/string.js
@@ -3,8 +3,18 @@
 const loss =50
 
 console.log(`hellomynameis${yname} and my loss is ${loss}`)*/
-const yname =new String("kartikkaushik")
+// new method of declaration of string and these are string prototypes
+const yname = new String("kartik-kaushik")
 console.log(yname.length)
-console.log(yname.split(4,8))
+
 console.log(yname.toLocaleLowerCase())
 console.log(yname.charAt(2))
+const newname = yname.substring(2,4)
+console.log(newname)
+const bname ="     kartik"
+console.log(bname)
+console.log(bname.trim())
+const url ="kartikhttps%20gmail"
+console.log(url.replace("%20"))
+console.log(url.includes("p"))
+console.log(yname.split("-"))
