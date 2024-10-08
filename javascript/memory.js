3e532f20c612e6628739690121bc16f821bd0a1f
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-08 19:25:34 +0000

    memory

---
+++ javascript/memory.js
@@ -0,0 +1,10 @@
+// stack memory block main value is always present in in selfname at any condition in stack memory
+
+/*let selfname =" kartik kaushik"
+
+let anothername = selfname
+anothername = "ankur"
+console.log(selfname)
+console.log(anothername)*/
+
+
