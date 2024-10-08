2edc590d297347fd40badc7984d84e4dcda168c0
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-08 20:07:02 +0000

    string

---
+++ javascript/string.js
@@ -0,0 +1,10 @@
+// string interpulation modern technique of adding strings
+/*const yname ="kartik"
+const loss =50
+
+console.log(`hellomynameis${yname} and my loss is ${loss}`)*/
+const yname =new String("kartikkaushik")
+console.log(yname.length)
+console.log(yname.split(4,8))
+console.log(yname.toLocaleLowerCase())
+console.log(yname.charAt(2))
