4abd906a135963a0f3ee98076d8d10faa4c8ccd1
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-08 19:39:58 +0000

    memory

--- javascript/memory.js
+++ javascript/memory.js
@@ -6,5 +6,17 @@ let anothername = selfname
 anothername = "ankur"
 console.log(selfname)
 console.log(anothername)*/
+// heap memory concept refernce value is taken not copy for eg if we change one value to another by pass it will change 
+
+let userone = {
+    email: "kartiikkaushik.com",
+    upi: "kartikybl.com"
+
+}
+let usertwo = userone
+usertwo.email ="subhashgosmwami.com"
+console.log(usertwo.email)
+console.log(userone.email)
+
 
 
