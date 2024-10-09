974d5f1462be91941e64448734897f99e10532ad
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-09 19:00:21 +0000

    nummaths

--- javascript/nummaths.js
+++ javascript/nummaths.js
@@ -8,3 +8,10 @@ console.log(newnum.toString().length)
 console.log(newnum.toFixed(3))
 console.log(othernum.toPrecision(15))
 console.log(newnum.toLocaleString('en-IN'))*/
+/*console.log(Math.random())// math ceil,abs,floor,random 
+console.log(Math.random()*10+1)
+console.log((Math.random()*10)+1)*/
+const min =10
+const max = 30
+// important rule
+console.log(Math.floor(Math.random()*(max-min+1)+min))
