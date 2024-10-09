88e9a0081253059559afe06417bb49ccab81b4f7
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-09 17:19:04 +0000

    nummaths.js

---
+++ javascript/nummaths.js
@@ -0,0 +1,10 @@
+/*console.log("kartikkaushik")
+const num = 40000000000
+console.log(400)
+const newnum = new Number(30000000000000)
+const othernum = 123666
+console.log(newnum)
+console.log(newnum.toString().length)
+console.log(newnum.toFixed(3))
+console.log(othernum.toPrecision(15))
+console.log(newnum.toLocaleString('en-IN'))*/
