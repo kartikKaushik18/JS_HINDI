98cd38064facdbeac32b59c25ec5a485689685a9
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-09 19:24:05 +0000

    dates

---
+++ javascript/dates.js
@@ -0,0 +1,7 @@
+let mydate = new Date(2023-09-18)
+console.log(mydate.toLocaleString())
+console.log(mydate.toDateString())
+let myTimestamp = Date.now()
+console.log(myTimestamp)
+console.log(mydate.getTime())
+console.log(Math.floor((Date.now()/1000)))
