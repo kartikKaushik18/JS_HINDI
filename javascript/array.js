d1ee230d44ca15b473fd151e8357d50662ad03aa
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-12 13:08:32 +0000

    array

---
+++ javascript/array.js
@@ -0,0 +1,11 @@
+const array1 = [1,2,3,4,5]
+//console.log(array1)
+array1.push(6)
+array1.push(8)
+//console.log(array1)
+const array2 =array1.slice(1,3)
+console.log(array1)
+console.log(array2)
+const array3 =array1.splice(1,3)
+console.log(array1)
+console.log(array3)
