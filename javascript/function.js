ac6659ca44c2837867bb2d4aa1c02abfe4dbfa8c
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-13 20:39:42 +0000

    functions

---
+++ javascript/function.js
@@ -0,0 +1,17 @@
+function myname(){
+console.log("k")
+console.log("k")
+console.log("k")
+console.log("k")
+console.log("k")
+}
+myname()
+function addtwonumbers(num1,num2){
+let result =num1+num2
+return result
+
+
+}
+addtwonumbers(8,9)
+const result =addtwonumbers(8,9)
+console.log(result)
