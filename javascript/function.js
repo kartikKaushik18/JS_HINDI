8ea09e99456c77b33b42a9cd6cc3e357e75bb8d9
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-14 06:17:06 +0000

    function

--- javascript/function.js
+++ javascript/function.js
@@ -15,3 +15,34 @@ return result
 addtwonumbers(8,9)
 const result =addtwonumbers(8,9)
 console.log(result)
+function justlogedin(username ='sam'){
+if(!username){
+console.log(" please enter your name")
+//return
+
+}
+
+
+    return `${username} justlogedin`
+
+}
+console.log(justlogedin())
+function cartshoping(...usernames){
+return usernames
+
+}
+console.log(cartshoping(" kartik kaushik"," kunal"," mannn singh"))
+const user ={
+    firstname:"hello",
+    lastname:"hi"
+}
+function objectpassing(anyobject){
+console.log(`${anyobject.firstname},${anyobjectlastname}`)
+return anyobject
+}
+//another method
+objectpassing({
+    firstname:"kartikkaushik",
+    lastname :"ankur"
+})
+console.log()
