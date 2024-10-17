5d06440f68fbbf654eaae57204839a59d699b0ec
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-17 11:25:07 +0000

    scope

--- javascript/scope.js
+++ javascript/scope.js
@@ -9,3 +9,17 @@ if(true){
 }
 console.log(b)
 console.log(c)
+//nested functions
+function login(){
+const username = "kartik"
+
+function anotherdetail() {
+  const age =18
+  const gender = "Male"
+  console.log(username)
+}
+anotherdetail()
+//console.log(age)
+
+}
+login()
