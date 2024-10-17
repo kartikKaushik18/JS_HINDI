e929444eb2841c49929d67187f7800efdcac0cbb
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-17 11:43:39 +0000

    scope

--- javascript/scope.js
+++ javascript/scope.js
@@ -23,3 +23,25 @@ anotherdetail()
 
 }
 login()
+if(true){
+    const fname = " kaushik"
+    if(fname === " kaushik"){
+        const favgame ="cricket"
+        const favmovie ="abcd"
+         console.log(fname)
+       console.log(favmovie)
+    }
+//console.log(favmovie)
+}
+console.log(addone(5))
+function addone(num){
+return num+1
+}
+// addtwo(5) wrong method in this case
+const addtwo = function addone(num){
+    return num+2
+    }
+
+console.log(addtwo(5))
+
+
