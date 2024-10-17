80e5818e67b5b9d7ba1afe18d63e505c1d8ea54e
Kartik Kaushik <kartikkaushik999999@gmail.com>
2024-10-17 20:18:11 +0000

    arrow

---
+++ javascript/arrow.js
@@ -0,0 +1,31 @@
+/*const addtwo =(num1,num2) =>{
+    return num1+num2
+}
+console.log(addtwo(3,4))
+// const addtwo =(num1,num2) => (num1+num2 )implicit function useful in react
+//const addtwo =(num1,num2) =>( { : }) in case of object to this method
+const chai =() =>{
+    login = " kartik kaushik"
+    console.log(this)// to check scope 
+}
+
+chai()*/
+// IIFE to overcome the global space problem 
+/*(function chaiiii(){
+
+console.log(" kartik");
+
+})();*/
+
+function hello(){
+    console.log("hello")
+}
+hello();
+
+(function chaiiii(){
+
+    console.log(" kartik");
+    
+    })();
+
+
